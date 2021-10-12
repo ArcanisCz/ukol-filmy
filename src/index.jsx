94d77fa115f9626b52eaf0e782347eb066f9398c
Tatiana Hodic <tatiana.hodic.miklos@gmail.com>
2021-10-12 22:08:02 +0200

    project done

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-import camera from './img/camera.svg';
+import Header from './components/Header/index';
+import Movie from './components/Movie/index';
+import movies from './movies';
 
-const App = () => (
-  <>
-    <h1>
-      <img class="logo" src={camera} alt="Logo" />
-      V našem kině právě uvádíme
-    </h1>
-  </>
-);
+const App = () => { 
+  console.log(movies);
+  return (
+    <>
+      <Header />
+      <div className="movies">
+        { movies.map(film => { return <Movie title={film.title} poster={'./assets/' + film.poster} year={film.year} rating={film.rating} director={film.director} genre={film.genre} /> }) }
+      </div>
+    </>
+  );
+}
 
 render(<App />, document.querySelector('#app'));
