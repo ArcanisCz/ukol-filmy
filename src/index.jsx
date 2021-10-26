bf5a8a7a7c566b6f103c09a6b0ab07b456c43779
Jana OK <jjanule@gmail.com>
2021-10-26 21:36:57 +0200

    ukol

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import movies from './movies'
 
-import camera from './img/camera.svg';
+import Header from './components/header';
+import MovieList from './components/movieList'
 
 const App = () => (
   <>
-    <h1>
-      <img class="logo" src={camera} alt="Logo" />
-      V našem kině právě uvádíme
-    </h1>
+   <Header />
+   <MovieList movies={movies} />
   </>
 );
 
