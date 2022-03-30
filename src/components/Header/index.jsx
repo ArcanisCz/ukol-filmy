9491d7106966d49096f965ed475e0547e5aa56ef
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-03-30 23:14:48 +0200

    addHeader

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import camera from './camera.svg';
+
+const Header = () => {
+    return (        
+    <h1>
+        <img class="logo" src={camera} alt="Logo" />
+        V našem kině právě uvádíme
+    </h1>  
+    )
+}
+
+export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import Header from './components/Header';
 
-import camera from './img/camera.svg';
+//import camera from './img/camera.svg';
 
 const App = () => (
   <>
-    <h1>
-      <img class="logo" src={camera} alt="Logo" />
-      V našem kině právě uvádíme
-    </h1>
+    <Header />
   </>
 );
 
