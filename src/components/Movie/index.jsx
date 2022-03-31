51afa4c6b7fb2112ce4999de8a6508f8a7c77edc
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-03-31 23:39:27 +0200

    add Movie component

---
+++ src/components/Movie/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Movie = ({title, poster, year, rating, director, genre, cast}) => {
+    return(<>
+        <h2 className='movie-title'>{title}</h2>
+        <img className='poster' src={`/assets/${poster}`} />
+        <p className='year'>Rok: {year}</p>
+        <p className='rating'>Hodnocení: {rating}</p>
+        <p className='director'>Režisér: {director}</p>
+        <p>{genre}</p>
+        </>
+    )
+}
+
+export default Movie;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import Header from './components/Header';
+import Movie from './components/Movie';
+import movies from './movies';
 
-//import camera from './img/camera.svg';
+
 
 const App = () => (
   <>
     <Header />
-  </>
+
+    {movies.map((movie) => <Movie title={movie.title} 
+    poster={movie.poster}
+    year={movie.year}
+    rating={movie.rating}
+    director={movie.director}
+    />)}
+      </>
 );
 
 render(<App />, document.querySelector('#app'));
