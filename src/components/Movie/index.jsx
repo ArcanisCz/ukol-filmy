bceb78a879ff6b4ee911cd68faedf813357d8796
Jirinka <jirina.poustecka@greenfoxacademy.com>
2022-04-05 19:08:56 +0200

    add grid layout

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './style.css';
 
 const Movie = ({title, poster, year, rating, director, genre, cast}) => {
     console.log(title);
-    return(<>
-        <h2 className='movie-title'>{title}</h2>
-        <img className='poster' src={`/assets/${poster}`} />
-        <p className='year'>Rok: {year}</p>
-        <p className='rating'>Hodnocení: {rating}</p>
-        <p className='director'>Režisér: {director}</p>
-        <p className='genre'>Žánr: {genre}</p>
-        </>
+    return(<div className='movie'>
+                <div className='grid-header'>
+                    <h2 className='movie-title'>{title}</h2>
+                </div>
+                <div className='left-column'>
+                    <img className='poster' src={`/assets/${poster}`} />
+                </div>
+                <div className='right-column'>
+                    <p className='year'>Rok: {year}</p>
+                    <p className='rating'>Hodnocení: {rating}</p>
+                    <p className='director'>Režisér: {director}</p>
+                    <p className='genre'>Žánr: {genre}</p>
+                </div>
+            </div>
     )
 }
 
